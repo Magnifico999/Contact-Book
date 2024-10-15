80631e73b4b75d34437644afab1640eff2c0ed99
Elmagn1f1co <steveamadi007@gmail.com>
2024-10-15 11:31:21 +0100

    Project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Models; 
+
+namespace Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Contact>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=myDatabase.db");
+        }*/
+
+
+        DbSet<Contact> Contacts { get; set; } 
+
+
+    }
+}
